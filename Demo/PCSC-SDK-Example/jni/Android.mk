60e2f7725fec77edcb8aae526e49d22e7df8567a
Feitian Technologies <hongbin@ftsafe.com>
2017-05-23 11:18:21 +0800

    Update pcscd service to 0.4 and sessioncontroller to 1.04
    1. Imrpove speed to get card slot status
    2. Solve the issue without pair with reader, then call pcscd, the App
    has delay 5s

--- Demo/PCSC-SDK-Example/jni/Android.mk
+++ Demo/PCSC-SDK-Example/jni/Android.mk
@@ -5,6 +5,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE	:=	PCSC
 LOCAL_SRC_FILES	:=	PCSC.c
 LOCAL_CFLAGS	:=	-I./depend -I./depend/PCSC -fno-short-enums
-LOCAL_LDLIBS	:=	-L./depend -lpcsclite32_0.1 -llog
+LOCAL_LDLIBS	:=	-L./depend -lpcsclite32_0.4 -llog
 
 include $(BUILD_SHARED_LIBRARY)
